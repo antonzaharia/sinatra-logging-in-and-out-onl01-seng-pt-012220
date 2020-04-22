87faffe0ed460ceb4c10ce85e300da952f93b404
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-04-22 16:48:13 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,9 +14,9 @@ class ApplicationController < Sinatra::Base
     @user = User.find_by(username: params["username"], password: params["password"])
     if @user
       session[:user_id] = @user.id
-      redirect to('/account')
+      erb :account
     end
-      redirect '/'
+      erb :error
   end
 
   get '/account' do
