d024ae87786799beab6fe030d770d4b51027f620
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-04-22 16:38:22 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,17 @@
 class User < ActiveRecord::Base
   attr_accessor :username, :password, :balance
 
+  @@all =[]
+
+  def initialize(username: username, password: password, balance: balance)
+    @username = username
+    @password = password
+    @balance = balance
+    @@all << self
+  end
+
+  def sefl.all
+    @@all
+  end
 
 end
