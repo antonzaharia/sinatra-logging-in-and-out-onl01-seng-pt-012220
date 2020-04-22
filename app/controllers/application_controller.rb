c684d12b857214e33ce68141a69003899bf27817
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-04-22 16:33:09 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,8 +12,9 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(username: params["username"], password: params["password"])
+    binding.pry
     if @user
-      binding.pry
+
       session[:user_id] = @user.id
 
     else
