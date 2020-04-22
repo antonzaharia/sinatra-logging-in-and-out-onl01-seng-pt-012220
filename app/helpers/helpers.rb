d71798b895faaa50f94873fc9f5319481f294e89
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-04-22 17:03:04 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,9 +1,8 @@
 class Helpers
 
   def current_user(session)
-    if session[:user_id] == @user
-      return @user
-    end
+    @user = session[:user_id]
+    @user
   end
 
   def is_logged_in?(session)
